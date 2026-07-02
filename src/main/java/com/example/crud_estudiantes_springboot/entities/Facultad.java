f74b7b9f07835aa351fe2f5eb51be0e1a3abb1d1
daniglezpereira <daniglezpereira@gmail.com>
2026-07-02 11:09:11 +0200

    actualizado

--- src/main/java/com/example/crud_estudiantes_springboot/entities/Facultad.java
+++ src/main/java/com/example/crud_estudiantes_springboot/entities/Facultad.java
@@ -1,23 +1,30 @@
 package com.example.crud_estudiantes_springboot.entities;
 
+import java.io.Serializable;
+import java.util.List;
+
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
 @Table(name = "facultades")
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
+@ToString(exclude = "profesores")
 @Builder
-public class Facultad {
+public class Facultad implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
 
-    @NotBlank
-    @Column(name = "nombre")
     private String nombre;
+
+    // Muchas facultades con varios profesores
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, mappedBy = "facultad")
+    private List<Profesor> profesores;
 }
--- src/main/java/com/example/crud_estudiantes_springboot/entities/Profesor.java
+++ src/main/java/com/example/crud_estudiantes_springboot/entities/Profesor.java
@@ -1,53 +1,64 @@
 package com.example.crud_estudiantes_springboot.entities;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.*;
-
+import java.io.Serializable;
+import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.example.crud_estudiantes_springboot.model.Genero;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
 @Entity
 @Table(name = "profesores")
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
+@ToString(exclude = "facultad")
 @Builder
-public class Profesor {
+public class Profesor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
 
-    @NotBlank
-    @Column(name = "nombre")
+    @NotNull(message = "El nombre no puede estar vacio")
+    @NotBlank(message = "El nombre no puede contener solamente espacios en blanco")
+    @Size(min = 2, max = 30, message = "El nombre tiene que estar entre 2 y 30 caracteres")
+    @Pattern(regexp = "^([A-ZÁÉÍÓÚÑ][a-záéíóúñ]+(\\s)?)+$",
+            message = "La primera letra en mayusculas y solo letras de la A a la Z")
     private String nombre;
 
-    @NotBlank
-    @Column(name = "primer_apellido")
+    @NotNull(message = "El primer apellido no puede estar vacio")
+    @NotBlank(message = "El primer apellido no puede contener solamente espacios en blanco")
+    @Size(min = 2, max = 30, message = "El primer apellido tiene que estar entre 2 y 30 caracteres")
+    @Pattern(regexp = "^([A-ZÁÉÍÓÚÑ][a-záéíóúñ]+(\\s)?)+$",
+            message = "La primera letra en mayusculas y solo letras de la A a la Z")
     private String primerApellido;
 
-    @NotBlank
-    @Column(name = "segundo_apellido")
+    @Size(max = 45, message = "El segundo apellido no puede superar los 45 caracteres")
+    @Pattern(regexp = "^([A-ZÁÉÍÓÚÑ][a-záéíóúñ]+(\\s)?)*$",
+            message = "La primera letra en mayusculas y solo letras de la A a la Z")
     private String segundoApellido;
 
-    @NotBlank
-    @Column(name = "genero")
-    private String genero;
+    @Enumerated(EnumType.STRING)
+    private Genero genero;
 
-    @NotNull
-    @Column(name = "fecha_alta")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @PastOrPresent(message = "La fecha de alta tiene que ser igual o anterior a la fecha actual")
     private LocalDate fechaAlta;
 
-    @NotNull
-    @Column(name = "salario")
-    private Double salario;
+    @NotNull(message = "El salario no puede estar vacio")
+    private BigDecimal salario;
 
     private String foto;
 
-    // Muchos profesores pertenecen a UNA facultad
-    @ManyToOne
-    @JoinColumn(name = "facultad_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
     private Facultad facultad;
 }
