45856aee2ccf7461638afa44116630a30c4f986b
daniglezpereira <daniglezpereira@gmail.com>
2026-07-02 12:56:14 +0200

    Añadidos detalles completos del profesor y pagina de inicio con acceso a estudiantes y profesores

--- src/main/java/com/example/crud_estudiantes_springboot/controllers/HomeController.java
+++ src/main/java/com/example/crud_estudiantes_springboot/controllers/HomeController.java
@@ -2,12 +2,13 @@ package com.example.crud_estudiantes_springboot.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-//Controlador de la página main 
+
+// Controlador de la página main
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "redirect:/estudiantes/listar";
+        return "index";
     }
 }
